374b091a9e7798bb7a59262035643733558e9c55
Dror <drorp24@gmail.com>
2018-07-01 22:05:13 +0300

    install styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
   ],
 }
--- src/pages/dror.js
+++ src/pages/dror.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Dror = () => <div style={{ color: 'black' }}>Hi I am Dror</div>
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+// Create a <Wrapper> react component that renders a <section> with
+// some padding and a papayawhip background
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
+
+const Dror = () => (
+  <div>
+    <Wrapper>
+      <Title>Hello World, this is my first styled component!</Title>
+    </Wrapper>
+  </div>
+)
 
 export default Dror
